fb2c94c65011a71b77b3c5751577cf6484434d20
Radina Kraeva <radinar.kraeva@gmail.com>
2021-03-28 19:12:04 +0300

    First Screen Modifications

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,8 @@
 import javafx.application.Application;
-import javafx.geometry.HPos;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.ColumnConstraints;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 
@@ -25,39 +17,10 @@ public class Main extends Application{
     public void start(Stage primaryStage) throws Exception {
         window = primaryStage;
         window.setTitle("Boston Metro System");
-
-        GridPane grid = new GridPane();
-        //grid.setPadding(new Insets (10, 10, 10, 10));
-        grid.setVgap(8);
-        grid.setHgap(10);
-        grid.setAlignment(Pos.CENTER);
-
-        ColumnConstraints col1 = new ColumnConstraints();
-        col1.setHalignment(HPos.LEFT);
-        col1.setPercentWidth(70);
-
-        ColumnConstraints col2 = new ColumnConstraints();
-        col2.setHalignment(HPos.RIGHT);
-        col2.setPercentWidth(30);
-
-        grid.getColumnConstraints().addAll(col1,col2);
-
-        HBox hbButtons = new HBox();
-        hbButtons.setSpacing(10.0);
-
-        Button btnFind = new Button("FIND ROUTE");
-        btnFind.setStyle("-fx-font-size: 15pt;");
-
-        Label lbl1 = new Label("WELCOME TO THE BOSTON METRO");
-        Label lbl2 = new Label("Find the fastest away around the city on a hill with ease!");
-
-        Image image = new Image("metro_map.png");
-        ImageView iv1 = new ImageView();
-        iv1.setImage(image);
-
-        Image image2 = new Image("boston_logo.png");
-        ImageView iv2 = new ImageView();
-        iv2.setImage(image2);
+        Parent root = FXMLLoader.load(getClass().getResource("FirstScreen.fxml"));
+        primaryStage.setTitle("Boston Metro System");
+        primaryStage.setScene(new Scene(root, 640, 400, Color.WHITE));
+        primaryStage.show();
 
 
         window.show();
